a4d581e784a5a05246f74f08f2d5ed37ca994f60
Wang Gang <withwanggang@gmail.com>
2023-03-02 23:17:11 +0800

    solution(go): add new solution for "6. Zigzag Conversion"

---
+++ alg/go/zigzagConversion/zigzagConversion2.go
@@ -0,0 +1,79 @@
+// Source : https://leetcode.cn/problems/zigzag-conversion
+// Date   : 2022-12-06
+
+/**********************************************************************************
+将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+请你实现这个将字符串进行指定行数变换的函数：
+
+string convert(string s, int numRows);
+
+示例 1：
+
+输入：s = "PAYPALISHIRING", numRows = 3
+输出："PAHNAPLSIIGYIR"
+示例 2：
+
+输入：s = "PAYPALISHIRING", numRows = 4
+输出："PINALSIGYAHRPI"
+解释：
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+
+示例 3：
+
+输入：s = "A", numRows = 1
+输出："A"
+
+
+提示：
+
+	1 <= s.length <= 1000
+	s 由英文字母（小写和大写）、',' 和 '.' 组成
+	1 <= numRows <= 1000
+**********************************************************************************/
+
+package zigzagConversion
+
+func convert2(s string, numRows int) string {
+	if numRows <= 1 {
+		return s
+	}
+
+	// character distance between columns
+	colStep := numRows + numRows - 2
+
+	// result
+	ss := make([]uint8, len(s), len(s))
+
+	// diagStep is distance from column to diagonal value.
+	// Reduces by two for each row
+	diagStep := colStep - 2
+
+	// i = position to write to in ss
+	i := 0
+	for row := 0; row < numRows; row++ {
+		// does this row have diagonal cells?
+		diag := row > 0 && row < numRows-1
+		for j := row; j < len(ss); j += colStep {
+			ss[i] = s[j] // column value
+			i += 1
+			if diag && j+diagStep < len(s) {
+				ss[i] = s[j+diagStep] // diagonal value
+				i += 1
+			}
+		}
+		if diag {
+			diagStep -= 2
+		}
+	}
+
+	return string(ss)
+}

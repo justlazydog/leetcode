3eac9b01638b0e4f682e7af361c37424321ca351
Wang Gang <withwanggang@gmail.com>
2023-03-09 12:17:54 +0800

    solution(go): "39. Combination Sum"

---
+++ alg/go/combinationSum/combinationSum.go
@@ -0,0 +1,74 @@
+// Source : https://leetcode.cn/problems/combination-sum
+// Date   : 2023-03-09
+
+/**********************************************************************************
+给你一个 无重复元素 的整数数组 candidates 和一个目标整数 target ，找出 candidates 中可以使数字和为目标数 target 的 所有 不同组合 ，并以列表形式返回。你可以按 任意顺序 返回这些组合。
+candidates 中的 同一个 数字可以 无限制重复被选取 。如果至少一个数字的被选数量不同，则两种组合是不同的。
+对于给定的输入，保证和为 target 的不同组合数少于 150 个。
+
+示例 1：
+
+输入：candidates = [2,3,6,7], target = 7
+输出：[[2,2,3],[7]]
+解释：
+2 和 3 可以形成一组候选，2 + 2 + 3 = 7 。注意 2 可以使用多次。
+7 也是一个候选， 7 = 7 。
+仅有这两种组合。
+示例 2：
+
+输入: candidates = [2,3,5], target = 8
+输出: [[2,2,2,2],[2,3,3],[3,5]]
+示例 3：
+
+输入: candidates = [2], target = 1
+输出: []
+
+
+提示：
+
+	1 <= candidates.length <= 30
+	2 <= candidates[i] <= 40
+	candidates 的所有元素 互不相同
+	1 <= target <= 40
+**********************************************************************************/
+
+package combinationSum
+
+/*
+	题目要求求出所有可以使数字和为 target 的组合，且 candidates 中的数字可以无限制重复被选取。这道题可以用回溯算法求解，具体过程如下：
+
+	对于每个数字，可以选取多次，每选取一次，将当前数字添加到组合中，并将目标数减去当前数字。
+
+	当目标数小于 0 时，说明当前组合已经不满足条件了，直接返回。
+
+	当目标数等于 0 时，说明当前组合已经满足条件，将组合加入结果集中，然后返回。
+
+	从当前数字的下一个位置开始继续搜索，直到 candidates 数组的末尾。
+*/
+
+func combinationSum(candidates []int, target int) [][]int {
+	var res [][]int
+	var combination []int
+	backtrack(candidates, target, combination, &res)
+	return res
+}
+
+func backtrack(candidates []int, target int, combination []int, res *[][]int) {
+	if target < 0 {
+		return
+	} else if target == 0 {
+		// 注意需要创建一个新的切片，否则后续修改 combination 会影响已加入结果集的组合
+		temp := make([]int, len(combination))
+		copy(temp, combination)
+		*res = append(*res, temp)
+	} else {
+		for i := 0; i < len(candidates); i++ {
+			// 在组合中添加当前数字
+			combination = append(combination, candidates[i])
+			// 继续从当前数字的位置开始搜索
+			backtrack(candidates[i:], target-candidates[i], combination, res)
+			// 撤销上一步操作
+			combination = combination[:len(combination)-1]
+		}
+	}
+}

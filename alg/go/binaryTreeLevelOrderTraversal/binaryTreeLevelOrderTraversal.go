dde37c6190ea6af5d1c89c9b9d019a26326fc3b0
Wang Gang <withwanggang@gmail.com>
2023-03-13 20:23:13 +0800

    solution(go): "102. Binary Tree Level Order Traversal"

---
+++ alg/go/binaryTreeLevelOrderTraversal/binaryTreeLevelOrderTraversal.go
@@ -0,0 +1,60 @@
+// Source : https://leetcode.cn/problems/binary-tree-level-order-traversal
+// Date   : 2023-03-13
+
+/**********************************************************************************
+给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+
+示例 1：
+
+输入：root = [3,9,20,null,null,15,7]
+输出：[[3],[9,20],[15,7]]
+
+示例 2：
+
+输入：root = [1]
+输出：[[1]]
+
+示例 3：
+
+输入：root = []
+输出：[]
+
+
+提示：
+
+	树中节点数目在范围 [0, 2000] 内
+	-1000 <= Node.val <= 1000
+**********************************************************************************/
+
+package binaryTreeLevelOrderTraversal
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return nil
+	}
+	var res [][]int
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		size := len(queue)
+		level := make([]int, size)
+		for i := 0; i < size; i++ {
+			node := queue[i]
+			level[i] = node.Val
+			if node.Left != nil {
+				queue = append(queue, node.Left)
+			}
+			if node.Right != nil {
+				queue = append(queue, node.Right)
+			}
+		}
+		queue = queue[size:]
+		res = append(res, level)
+	}
+	return res
+}
